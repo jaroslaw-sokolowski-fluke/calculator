1891afd51f7bb1d8f4fe63153488335ff831ae6f
jsokolow <jaroslaw.sokolowski@fluke.com>
2024-06-13 14:18:49 +0200

    Add sum logic, controller and unit test

---
+++ src/main/java/com/jsokolow/calculator/Calculator.java
@@ -0,0 +1,8 @@
+package com.jsokolow.calculator;
+import org.springframework.stereotype.Service;
+@Service
+public class Calculator {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+}
